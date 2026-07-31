542c4db2e1661fb5baa28eaa421114ea8ae01091
isabellaCordeiroet <bomfim.isabella@escola.pr.gov.br>
2026-07-31 08:50:45 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,20 @@
 
+// script.js
+
+document.addEventListener("DOMContentLoaded", () => {
+  alert("👽 Bem-vindo ao Blog sobre ETs!");
+
+  const titulo = document.querySelector("h1");
+
+  titulo.addEventListener("click", () => {
+    const mensagens = [
+      "🛸 Você encontrou um sinal do espaço!",
+      "👽 Será que estamos sozinhos no universo?",
+      "🌌 O universo é cheio de mistérios!",
+      "✨ Continue explorando o blog!"
+    ];
+
+    const aleatoria = mensagens[Math.floor(Math.random() * mensagens.length)];
+    alert(aleatoria);
+  });
+});
